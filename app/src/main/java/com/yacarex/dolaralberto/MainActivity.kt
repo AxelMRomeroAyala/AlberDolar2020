114c6b75a80530e3a45f20f495564fb6517eb052
Luciano Pereira <lucianop.pereira20@gmail.com>
2020-09-20 22:36:05 -0300

    Design + Logic Complete

--- app/src/main/java/com/yacarex/dolaralberto/MainActivity.kt
+++ app/src/main/java/com/yacarex/dolaralberto/MainActivity.kt
@@ -4,32 +4,69 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import androidx.core.widget.doAfterTextChanged
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
+import android.view.View
+import android.widget.*
+import androidx.core.view.size
 import com.android.volley.toolbox.Volley
-import com.google.android.material.snackbar.Snackbar
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.spinner_item.view.*
 import java.math.BigDecimal
 import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var option: Spinner
+    lateinit var selectedCurrency: String
+    lateinit var exchange: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val valueInput = findViewById<TextView>(R.id.input_value)
+        val valueOfficialClean = findViewById<TextView>(R.id.official_clean_value)
         val valueSolidarity = findViewById<TextView>(R.id.solidarity_value)
-        val valueClaimLeater = findViewById<TextView>(R.id.claim_leater_value)
+        val valueClaimLater = findViewById<TextView>(R.id.claim_later_value)
         val totalValue = findViewById<TextView>(R.id.total_value)
 
         getDolarValue()
+//        exchange = volley_value.text.toString().replace(",", ".")
+
+        option = findViewById(R.id.currency)
+        val options = arrayOf(
+            getString(R.string.currency_usa),
+            getString(R.string.currency_arg)
+        )
+
+        option.adapter = ArrayAdapter<String>(
+            this,
+            R.layout.spinner_item,
+            options
+        )
+
+
+        option.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                selectedCurrency = options[position]
+                input_value.setText("")
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                option.setSelection(0)
+            }
+
+        }
+
+
 
         valueInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -38,11 +75,14 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
 
-                val input = if(valueInput.text.toString().isEmpty()) 0.0 else valueInput.text.toString().toDouble()
-                val values: ArrayList<String> = fillValues(input)
-                valueSolidarity.text = values.get(0)
-                valueClaimLeater.text = values.get(1)
-                totalValue.text = values.get(2)
+                val input =
+                    if (valueInput.text.toString().isEmpty()) 0.0 else valueInput.text.toString()
+                        .toDouble()
+                val values: ArrayList<String> = fillValues(input, selectedCurrency)
+                valueOfficialClean.text = values[0]
+                valueSolidarity.text = values[1]
+                valueClaimLater.text = values[2]
+                totalValue.text = values[3]
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -53,58 +93,135 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun getTotal(input: Double): BigDecimal? {
-        var usdChange = input * 80
-        var total = BigDecimal(usdChange + getSolidarity(input) + getClaimLeater(input)).setScale(
-            2,
-            RoundingMode.HALF_EVEN
-        )
+    private fun getArsTotal(input: Double): Double {
 
-        return total
+        return getOfficial(input) + getSolidarity(input) + getClaimLater(input)
+    }
+
+    private fun getUsdTotal(input: Double): Double? {
+        val conversion = volley_value.text.toString().replace(",", ".", false).toDouble()
+        return (input / 1.65) / conversion
+    }
+
+    private fun getOfficial(input: Double): Double {
+        val conversion = volley_value.text.toString().replace(",", ".", false).toDouble()
+        return input * conversion
+    }
+
+    private fun getOfficialInverted(input: Double): Double {
+        val conversion = volley_value.text.toString().replace(",", ".", false).toDouble()
+        return BigDecimal(
+            input / 1.65 / conversion
+        ).setScale(
+            2,
+            RoundingMode.CEILING
+        ).toDouble()
     }
 
     private fun getSolidarity(input: Double): Double {
-        var usdChange = input * 80
-        var solidarity = usdChange * 0.3
-        return solidarity
+        return getOfficial(input) * 0.3
     }
 
-    private fun getClaimLeater(input: Double): Double {
-        var usdChange = input * 80
-        var claimLeater = usdChange * 0.35
-        return claimLeater
+    private fun getSolidarityInverted(input: Double): Double {
+        return getSolidarity(getUsdTotal(input)!!)
     }
 
-    private fun fillValues(number: Double): ArrayList<String> {
+    private fun getClaimLater(input: Double): Double {
+        return getOfficial(input) * 0.35
+    }
+
+    private fun getClaimLaterInverted(input: Double): Double {
+        return getOfficial(getUsdTotal(input)!!) * 0.35
+    }
+
+    private fun fillValues(number: Double, currency: String): ArrayList<String> {
         val results: ArrayList<String>
-        if ((number <= 0) || number == null) {
+        if (number <= 0) {
 
-            results = ArrayList(3)
+            results = ArrayList(4)
             results.add(0, "AR$ " + getString(R.string.value_empty))
             results.add(1, "AR$ " + getString(R.string.value_empty))
             results.add(2, "AR$ " + getString(R.string.value_empty))
+            results.add(
+                3,
+                (if (currency == getString(R.string.currency_usa)) "AR$ " else "U\$D"
+                        ) + " " + getString(R.string.value_empty)
+            )
             return results
 
-        } else if (number > 200) {
+        } else if ((currency == getString(R.string.currency_usa) && number > 200) ||
+            currency == getString(R.string.currency_arg) && getUsdTotal(number)!!.toDouble() > 200
+        ) {
 
-            results = ArrayList(3)
+            results = ArrayList(4)
             results.add(0, "AR$ " + getString(R.string.value_empty))
             results.add(1, "AR$ " + getString(R.string.value_empty))
-            results.add(2, getString(R.string.value_exceded))
+            results.add(2, "AR$ " + getString(R.string.value_empty))
+            results.add(3, getString(R.string.value_exceded))
             return results
 
         } else {
 
-            results = ArrayList(3)
-            results.add(0, "AR$ ${getSolidarity(number)}")
-            results.add(1, "AR$ ${getClaimLeater(number)}")
-            results.add(2, "AR$ ${getTotal(number)}")
+            results = ArrayList(4)
+
+            when (currency) {
+                getString(R.string.currency_usa) -> {
+                    results.add(
+                        0,
+                        getString(R.string.currency_arg) + " " + BigDecimal(getOfficial(number)).setScale(
+                            2,
+                            RoundingMode.CEILING
+                        )
+                    )
+                    results.add(
+                        1,
+                        getString(R.string.currency_arg) + " " + BigDecimal(getSolidarity(number)).setScale(
+                            2,
+                            RoundingMode.CEILING
+                        )
+                    )
+                    results.add(
+                        2,
+                        getString(R.string.currency_arg) + " " + BigDecimal(getClaimLater(number)).setScale(
+                            2,
+                            RoundingMode.CEILING
+                        )
+                    )
+                    results.add(
+                        3,
+                        getString(R.string.currency_arg) + " " + BigDecimal(getArsTotal(number)).setScale(
+                            2,
+                            RoundingMode.CEILING
+                        )
+                    )
+                }
+
+                getString(R.string.currency_arg) -> {
+                    results.add(
+                        0,
+                        getString(R.string.currency_arg) + " " + getOfficialInverted(number)
+                    )
+                    results.add(
+                        1,
+                        getString(R.string.currency_arg) + " " + getSolidarityInverted(number)
+                    )
+                    results.add(
+                        2,
+                        getString(R.string.currency_arg) + " " + getClaimLaterInverted(number)
+                    )
+                    results.add(
+                        3,
+                        getString(R.string.currency_usa) + " " + getUsdTotal(number)
+                    )
+                }
+
+            }
             return results
 
         }
     }
 
-    private fun getDolarValue(){
+    private fun getDolarValue() {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
         val url = "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
@@ -112,19 +229,20 @@ class MainActivity : AppCompatActivity() {
 // Request a string response from the provided URL.
         val stringRequest = StringRequest(
             Request.Method.GET, url,
-            Response.Listener<String> { response ->
+            { response ->
                 // Display the first 500 characters of the response string.
-                val sType = object : TypeToken<List<DolarHomeModel>>() { }.type
+                val sType = object : TypeToken<List<DolarHomeModel>>() {}.type
                 val otherList = Gson().fromJson<List<DolarHomeModel>>(response, sType)
-                    volley_value.text = otherList.get(0).casa?.compra
+                volley_value.text = otherList[0].casa?.venta
             },
-            Response.ErrorListener {
-                volley_value.text = "ERROR"
+            {
+                volley_value.text = getString(R.string.value_empty)
             })
 
 // Add the request to the RequestQueue.
         queue.add(stringRequest)
 
     }
+
 }
 
