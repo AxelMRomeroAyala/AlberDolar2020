e887df33839dc45b949f7fc9ff842981450d9d03
Luciano Pereira <lucianop.pereira20@gmail.com>
2020-10-01 20:29:11 -0300

    Fixes for update release

--- app/src/main/java/com/yacarex/dolaralberto/MainActivity.kt
+++ app/src/main/java/com/yacarex/dolaralberto/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.yacarex.dolaralberto
 
+import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.android.volley.toolbox.Volley
 import com.android.volley.Request
@@ -18,6 +21,7 @@ import com.google.android.gms.ads.MobileAds
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.edit_official_layout.*
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.text.DecimalFormat
@@ -31,17 +35,37 @@ class MainActivity : AppCompatActivity() {
     lateinit var resultCurrency: String
     private lateinit var mAdView: AdView
 
+    @SuppressLint("InflateParams")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val colorStateList = ColorStateList(
-            arrayOf(
-                intArrayOf(-android.R.attr.state_enabled),
-                intArrayOf(android.R.attr.state_enabled)
-            ),
-            intArrayOf(Color.BLACK, Color.GREEN)
-        )
+        edit_button.setOnClickListener {
+
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val editConversionDialog = inflater.inflate(R.layout.edit_official_layout, null)
+            val insertOfficial = editConversionDialog.findViewById<EditText>(R.id.insert_official)
+
+            with(builder) {
+
+                setTitle("Que valor de conversión usarás?")
+
+                setPositiveButton("OK") { _, _ ->
+                    volley_value.text = insertOfficial.text.toString()
+                }
+
+                setNegativeButton("Cancelar") { _, _ ->
+
+                }
+                setNeutralButton("Restaurar") { _, _ ->
+                    getDolarValue()
+                }
+                setView(editConversionDialog)
+                show()
+
+            }
+        }
 
         getDolarValue()
 
@@ -49,25 +73,10 @@ class MainActivity : AppCompatActivity() {
 
         mAdView = findViewById(R.id.adView)
         val adRequest =
-            AdRequest.Builder().addTestDevice("7790A97E03F01714C1A757BAC702560D").build()
+            AdRequest.Builder().build()
         mAdView.loadAd(adRequest)
 
         // View control on mode selection in RadioGroup
-
-        switch_edit.setOnCheckedChangeListener { _, _ ->
-            if (switch_edit.isChecked) {
-
-                input_official.visibility = View.VISIBLE
-                volley_value.visibility = View.GONE
-
-            } else {
-
-                input_official.visibility = View.GONE
-                volley_value.visibility = View.VISIBLE
-
-            }
-
-        }
 
         mode_select.check(R.id.mode_direct)
         mode_select.setOnCheckedChangeListener { _, checkedId ->
@@ -76,13 +85,12 @@ class MainActivity : AppCompatActivity() {
 
                 R.id.mode_direct -> {
 
-                    mode_direct.setButtonDrawable(R.drawable.ic_money_selected)
-                    mode_credit.setButtonDrawable(R.drawable.ic_credit_card)
-                    mode_digital_services.setButtonDrawable(R.drawable.ic_videogame)
                     mode_text.text = getString(R.string.mode_text_direct_buy)
                     iva_field.visibility = View.GONE
                     iva.visibility = View.GONE
                     warn_cannot_buy.visibility = View.GONE
+                    total_value.visibility = View.VISIBLE
+                    no_you_cant.visibility = View.GONE
 
                     when (selectedCurrency) {
 
@@ -91,42 +99,38 @@ class MainActivity : AppCompatActivity() {
 
                     }
 
-                    input_value.text.clear()
+                    input_value.text = input_value.text
 
                 }
 
                 R.id.mode_credit -> {
 
-                    mode_direct.setButtonDrawable(R.drawable.ic_money_unselected)
-                    mode_credit.setButtonDrawable(R.drawable.ic_credit_card_selected)
-                    mode_digital_services.setButtonDrawable(R.drawable.ic_videogame)
                     mode_text.text = getString(R.string.mode_text_credit_card)
                     iva_field.visibility = View.GONE
                     iva.visibility = View.GONE
                     text_total.text = getString(R.string.total_credit)
                     warn_cannot_buy.visibility = View.VISIBLE
+                    total_value.visibility = View.VISIBLE
+                    no_you_cant.visibility = View.GONE
 
-                    input_value.text.clear()
+                    input_value.text = input_value.text
 
                 }
 
                 R.id.mode_digital_services -> {
 
-                    mode_direct.setButtonDrawable(R.drawable.ic_money_unselected)
-                    mode_credit.setButtonDrawable(R.drawable.ic_credit_card)
-                    mode_digital_services.setButtonDrawable(R.drawable.ic_videogame_selected)
                     mode_text.text = getString(R.string.mode_text_digital_services)
                     iva_field.visibility = View.VISIBLE
                     iva.visibility = View.VISIBLE
                     text_total.text = getString(R.string.total_digital)
                     warn_cannot_buy.visibility = View.VISIBLE
+                    total_value.visibility = View.VISIBLE
+                    no_you_cant.visibility = View.GONE
 
-                    input_value.text.clear()
+                    input_value.text = input_value.text
 
                 }
-
             }
-
         }
 
         // Input Method Spinner
@@ -151,7 +155,6 @@ class MainActivity : AppCompatActivity() {
                 id: Long
             ) {
                 selectedCurrency = options[position]
-                setResultCurrency()
 
                 if (mode_direct.isChecked) when (selectedCurrency) {
 
@@ -173,6 +176,7 @@ class MainActivity : AppCompatActivity() {
 
         input_value.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
 
             }
 
@@ -205,27 +209,9 @@ class MainActivity : AppCompatActivity() {
             override fun afterTextChanged(s: Editable?) {
 
             }
-
         })
 
         //When Official input changes
-
-        input_official.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-                input_value.text.clear()
-
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-
-        })
 
     }
 
@@ -237,26 +223,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setResultCurrency() {
-        when {
-
-            mode_direct.isChecked && selectedCurrency == "AR$" -> {
-                resultCurrency = getString(R.string.currency_usa)
+    private fun getCurrency(): String {
+        return if (mode_direct.isChecked && selectedCurrency == "AR$") {
+                getString(R.string.currency_usa)
+            } else{
+            getString(R.string.currency_arg)
             }
-
-            mode_direct.isChecked && selectedCurrency == "U\$D" -> {
-                resultCurrency = getString(R.string.currency_arg)
-            }
-
-            mode_credit.isChecked -> {
-                resultCurrency = getString(R.string.currency_arg)
-            }
-
-            mode_digital_services.isChecked -> {
-                resultCurrency = getString(R.string.currency_arg)
-            }
-
-        }
     }
 
     private fun giveItRounded(toRound: Double): String {
@@ -277,50 +249,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getArsDigital(input: Double): Double {
-
         return if (selectedCurrency == "AR$") {
             input + getSolidarity(input) + getClaimLater(input) + getIva(input)
         } else {
             getDirectOfficial(input) + getSolidarity(input) + getClaimLater(input) + getIva(input)
         }
-
     }
-
 
     private fun getUsdTotal(input: Double): Double {
         return if (mode_select.checkedRadioButtonId == R.id.mode_digital_services) {
-
             BigDecimal(input / (1 + 0.35 + 0.08 + 0.21) / getConversion())
                 .setScale(2, RoundingMode.CEILING).toDouble()
-
         } else {
-
             BigDecimal(input / 1.65 / getConversion())
                 .setScale(2, RoundingMode.CEILING).toDouble()
-
         }
     }
 
     private fun getConversion(): Double {
-
-        return if (switch_edit.isChecked) {
-            input_official.text.toString().toDouble()
-        } else {
-            volley_value.text.toString().replace(",", ".", false).toDouble()
-        }
-
+        return volley_value.text.toString().replace(",", ".", false).toDouble()
     }
 
     private fun getDirectOfficial(input: Double): Double {
-
         return if (selectedCurrency == "U\$D") input * getConversion() else input
     }
 
 
     private fun getDirectOfficialInverted(input: Double): Double {
-
-        return getDirectOfficial(getUsdTotal(input))
-
+        return getUsdTotal(input) * getConversion()
     }
 
     private fun getSolidarity(input: Double): Double {
@@ -379,105 +335,119 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
 
+    private fun fillEmpty(number: Double): ArrayList<String>{
+
+        val results: ArrayList<String> = ArrayList(4)
+
+        results.add(0, "AR$" + getString(R.string.value_empty))
+        results.add(1, "AR$" + getString(R.string.value_empty))
+        results.add(2, "AR$" + getString(R.string.value_empty))
+        results.add(3, getCurrency() + getString(R.string.value_empty))
+        results.add(4, "AR$" + getString(R.string.value_empty))
+        setLimitReduction(0.0)
+
+        return results
+    }
+
+    private fun fillDirect(number: Double):ArrayList<String> {
+
+        val results: ArrayList<String> = ArrayList(4)
+
+        if ((selectedCurrency == "U\$D" && number > 200) ||
+            (selectedCurrency == "AR$" && getUsdTotal(number) > 200)
+        ) {
+
+            results.add(0, "AR$" + getString(R.string.value_empty))
+            results.add(1, "AR$" + getString(R.string.value_empty))
+            results.add(2, "AR$" + getString(R.string.value_empty))
+            results.add(3, getString(R.string.value_exceded))
+            results.add(4, "AR$" + getString(R.string.value_empty))
+            total_value.visibility = View.GONE
+            no_you_cant.visibility = View.VISIBLE
+
+        } else {
+
+            when (selectedCurrency) {
+                getString(R.string.currency_usa) -> {
+                    results.add(0, "AR$" + giveItRounded(getDirectOfficial(number)))
+                    results.add(1, "AR$" + giveItRounded(getSolidarity(number)))
+                    results.add(2, "AR$" + giveItRounded(getClaimLater(number)))
+                    results.add(3, getCurrency() + giveItRounded(getArsDirect(number)))
+                    results.add(4, "AR$" + giveItRounded(getIva(number)))
+                    total_value.visibility = View.VISIBLE
+                    no_you_cant.visibility = View.GONE
+                }
+
+                getString(R.string.currency_arg) -> {
+                    results.add(
+                        0,
+                        "AR$" + giveItRounded(getDirectOfficialInverted(number))
+                    )
+                    results.add(1, "AR$" + giveItRounded(getSolidarityInverted(number)))
+                    results.add(2, "AR$" + giveItRounded(getClaimLaterInverted(number)))
+                    results.add(3, getCurrency() + giveItRounded(getUsdTotal(number)))
+                    results.add(4, "AR$" + giveItRounded(getIvaInverted(number)))
+                    total_value.visibility = View.VISIBLE
+                    no_you_cant.visibility = View.GONE
+                }
+            }
+        }
+
+        return results
+    }
+
+    private fun fillCreditCard (number: Double): ArrayList<String>{
+
+        val results: ArrayList<String> = ArrayList(4)
+
+        results.add(0, "AR$" + giveItRounded(getDirectOfficial(number)))
+        results.add(1, "AR$" + giveItRounded(getSolidarity(number)))
+        results.add(2, "AR$" + giveItRounded(getClaimLater(number)))
+        results.add(3, "AR$" + giveItRounded(getArsCredit(number)))
+        results.add(4, "AR$" + giveItRounded(getIva(number)))
+        setLimitReduction(
+            if (selectedCurrency == "AR$") BigDecimal(number / getConversion()).setScale(
+                2,
+                RoundingMode.CEILING
+            ).toDouble() else number
+        )
+
+        return results
+    }
+
+    private fun fillDigitalServices(number: Double): ArrayList<String>{
+
+        val results: ArrayList<String> = ArrayList(4)
+
+        results.add(0, "AR$" + giveItRounded(getDirectOfficial(number)))
+        results.add(1, "AR$" + giveItRounded(getSolidarity(number)))
+        results.add(2, "AR$" + giveItRounded(getClaimLater(number)))
+        results.add(3, "AR$" + giveItRounded(getArsDigital(number)))
+        results.add(4, "AR$" + giveItRounded(getIva(number)))
+        setLimitReduction(
+            if (selectedCurrency == "AR$") BigDecimal(number / getConversion()).setScale(
+                2,
+                RoundingMode.CEILING
+            ).toDouble() else number
+        )
+
+        return results
     }
 
     private fun fillValues(number: Double): ArrayList<String> {
 
-        val results: ArrayList<String> = ArrayList(4)
+        var results: ArrayList<String> = ArrayList()
 
         when {
-            number <= 0 -> {
-
-                results.add(0, "AR$" + getString(R.string.value_empty))
-                results.add(1, "AR$" + getString(R.string.value_empty))
-                results.add(2, "AR$" + getString(R.string.value_empty))
-                results.add(3, resultCurrency + getString(R.string.value_empty))
-                results.add(4, "AR$" + getString(R.string.value_empty))
-                setLimitReduction(0.0)
-
-            }
-
-            mode_direct.isChecked -> {
-
-                if ((selectedCurrency == "U\$D" && number > 200) ||
-                    (selectedCurrency == "AR$" && getUsdTotal(number) > 200)
-                ) {
-
-                    results.add(0, "AR$ " + getString(R.string.value_empty))
-                    results.add(1, "AR$ " + getString(R.string.value_empty))
-                    results.add(2, "AR$ " + getString(R.string.value_empty))
-                    results.add(3, getString(R.string.value_exceded))
-                    results.add(4, "AR$ " + getString(R.string.value_empty))
-
-                } else {
-
-                    when (selectedCurrency) {
-                        getString(R.string.currency_usa) -> {
-                            results.add(0, "AR$ " + giveItRounded(getDirectOfficial(number)))
-                            results.add(1, "AR$ " + giveItRounded(getSolidarity(number)))
-                            results.add(2, "AR$ " + giveItRounded(getClaimLater(number)))
-                            results.add(3, "AR$ " + giveItRounded(getArsDirect(number)))
-                            results.add(4, "AR$ " + giveItRounded(getIva(number)))
-                        }
-
-                        getString(R.string.currency_arg) -> {
-                            results.add(
-                                0,
-                                "AR$ " + giveItRounded(getDirectOfficialInverted(number))
-                            )
-                            results.add(1, "AR$ " + giveItRounded(getSolidarityInverted(number)))
-                            results.add(2, "AR$ " + giveItRounded(getClaimLaterInverted(number)))
-                            results.add(3, "AR$ " + giveItRounded(getUsdTotal(number)))
-                            results.add(
-                                4,
-                                getString(R.string.currency_arg) + giveItRounded(
-                                    getIvaInverted(number)
-                                )
-                            )
-                        }
-                    }
-                }
-            }
-
-            mode_credit.isChecked -> {
-
-                results.add(0, "AR$" + giveItRounded(getDirectOfficial(number)))
-                results.add(1, "AR$" + giveItRounded(getSolidarity(number)))
-                results.add(2, "AR$" + giveItRounded(getClaimLater(number)))
-                results.add(3, "AR$" + giveItRounded(getArsCredit(number)))
-                results.add(4, "AR$" + giveItRounded(getIva(number)))
-                setLimitReduction(
-                    if (selectedCurrency == "AR$") BigDecimal(number / getConversion()).setScale(
-                        2,
-                        RoundingMode.CEILING
-                    ).toDouble() else number
-                )
-
-
-            }
-
-            mode_digital_services.isChecked -> {
-
-                results.add(0, "AR$" + giveItRounded(getDirectOfficial(number)))
-                results.add(1, "AR$" + giveItRounded(getSolidarity(number)))
-                results.add(2, "AR$" + giveItRounded(getClaimLater(number)))
-                results.add(3, "AR$" + giveItRounded(getArsDigital(number)))
-                results.add(4, "AR$" + giveItRounded(getIva(number)))
-                setLimitReduction(
-                    if (selectedCurrency == "AR$") BigDecimal(number / getConversion()).setScale(
-                        2,
-                        RoundingMode.CEILING
-                    ).toDouble() else number
-                )
-
-            }
-
-
+            number <= 0 -> results = fillEmpty(number)
+            mode_direct.isChecked -> results = fillDirect(number)
+            mode_credit.isChecked -> results = fillCreditCard(number)
+            mode_digital_services.isChecked -> results = fillDigitalServices(number)
         }
 
         return results
-
     }
 
     private fun getDolarValue() {
@@ -497,6 +467,7 @@ class MainActivity : AppCompatActivity() {
             {
                 volley_value.text = getString(R.string.value_empty)
             })
+
 
 // Add the request to the RequestQueue.
         queue.add(stringRequest)
